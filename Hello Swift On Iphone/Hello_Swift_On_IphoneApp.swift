20a3f676daf09cef4a875438bc1fc9ac88da3cc6
kaze11koa <kaze11koa@gmail.com>
2021-08-05 15:44:56 +0900

    Initial Commit

---
+++ Hello Swift On Iphone/Hello_Swift_On_IphoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hello_Swift_On_IphoneApp.swift
+//  Hello Swift On Iphone
+//
+//  Created by SEO.koa on 2021/08/05.
+//
+
+import SwiftUI
+
+@main
+struct Hello_Swift_On_IphoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
