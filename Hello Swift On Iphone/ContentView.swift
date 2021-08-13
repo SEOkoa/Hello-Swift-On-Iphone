1a8ee605e06cbfee010ba6243d5700cdf926e198
SEOkoa <kaze11koa@gmail.com>
2021-08-14 00:33:24 +0900

    first step) SWIFT UI

--- Hello Swift On Iphone/ContentView.swift
+++ Hello Swift On Iphone/ContentView.swift
@@ -9,13 +9,41 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("무야호")
-            .padding()
+        
+        VStack {
+            VStack {
+                VStack(alignment: .center) {
+                
+                }
+            }
+           
+            Image("padaeng1")
+                
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                
+            
+            Link("XROSS", destination: URL(string: "https://xross.kr")!)
+            
+            NavigationView {
+                
+                NavigationLink(destination: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Destination@*/Text("Destination")/*@END_MENU_TOKEN@*/) {
+                    
+                   
+                   
+                    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Content@*/Text("Navigate")/*@END_MENU_TOKEN@*/ }
+            }
+            .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            .previewDevice("iPhone 12 mini")
     }
 }
